b5a70f83c504f0b516f10e897d7314203fb5454a
Taotao <taotao@joycastle.mobi>
2023-03-01 20:20:11 +0800

    :art; 增加url

--- myproject/myapp/views.py
+++
@@ -1,6 +0,0 @@
-# Create your views here.
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Hello, world!")
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""myproject URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.urls import path
-from myapp.views import home
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('home/', home, name='home'),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
