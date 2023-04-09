ff29969a5fc15b43fc8c52084118645a486115ee
Taotao <taotao@joycastle.mobi>
2023-04-09 22:23:38 +0800

    :art: 配置数据库

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django@localhost',
+        'USER': 'django',
+        'PASSWORD': 'Ht@123456',      #密码
+        'HOST': '127.0.0.1',    #IP
+        'PORT': '3306',                   #端口
     }
 }
 
